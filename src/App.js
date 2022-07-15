abe3a9a6fed2b1c0469ed36cd26b9b14c0b28bce
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2022-07-15 22:12:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./styles.css";
+
+var emojiDistnory = {
+  "🙂": "smiling",
+  "😔": "sad",
+  "❤️": "heart",
+  "😡": "angry",
+  "😭": "crying",
+  "😂": "laugh",
+  "😍": "smiling face with heart eyes",
+  "😗": "kissing face",
+  "🙄": "face with rolling eyes"
+};
+var emojiweKnow = Object.keys(emojiDistnory);
+
+export default function App() {
+  const [meaning, setmeaning] = useState("");
+  function textChange(event) {
+    var input = event.target.value;
+    var meaning = emojiDistnory[input];
+    if (meaning === undefined) {
+      meaning = "enter enoji";
+    }
+    setmeaning(meaning);
+  }
+  function emojiHandler(emoji) {
+    var meaning = emojiDistnory[emoji];
+    setmeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>inside outt</h1>
+      <input onChange={textChange} />
+      <h2>{meaning}</h2>
+      <h3>Emojis we know</h3>
+      {emojiweKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiHandler(emoji)}
+            key={emoji}
+            style={{ cursor: "pointer", padding: "1rem", fontSize: "20px" }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
